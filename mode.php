ab1ca2b760eaf227de32a90b5f648ffc16950ffe
Sui <chensuim@126.com>
2016-08-30 23:14:32 +0800

    settings

---
+++ mode.php
@@ -0,0 +1,35 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "murder");
+
+
+
+$pattern_mode='/^(m|M)ode[1-9]{1}/';
+$pattern_name='/^(n|N)ame/';
+
+$mode_repository='12';
+
+
+
+$repository="
+1.抵抗组织阿瓦隆（包含抵抗组织初版）；
+2.狼人杀。
+";
+
+
+
+$mode_name=[
+	1=>'抵抗组织阿瓦隆',
+	2=>'狼人杀'
+];
+
+$textTpl = "<xml>
+	 <ToUserName><![CDATA[%s]]></ToUserName>
+         <FromUserName><![CDATA[%s]]></FromUserName>
+         <CreateTime>%s</CreateTime>
+         <MsgType><![CDATA[text]]></MsgType>
+         <Content><![CDATA[%s]]></Content>
+         <FuncFlag>0</FuncFlag>
+         </xml>";
